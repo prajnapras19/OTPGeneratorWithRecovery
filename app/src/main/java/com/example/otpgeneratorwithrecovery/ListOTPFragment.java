08a7b05ad661e5409bcf8e77ee6da0dbd021d9dc
Prajna Prasetya <prajnapras19@gmail.com>
2023-04-20 11:28:59 +0700

    feat: list otp

--- app/src/main/java/com/example/otpgeneratorwithrecovery/ListOTPFragment.java
+++ app/src/main/java/com/example/otpgeneratorwithrecovery/ListOTPFragment.java
@@ -1,7 +1,69 @@
 package com.example.otpgeneratorwithrecovery;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.otpgeneratorwithrecovery.crypto.OTPSecret;
+import com.example.otpgeneratorwithrecovery.databinding.FragmentListOtpBinding;
+import com.example.otpgeneratorwithrecovery.util.Util;
+
+import java.util.Map;
+
 public class ListOTPFragment  extends Fragment {
-    // TODO
+    private FragmentListOtpBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentListOtpBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SharedPreferences sharedPref = getContext().getSharedPreferences(
+                getString(R.string.otp_secret_shared_preferences_file), Context.MODE_PRIVATE);
+        Map<String, ?> otpSecrets = sharedPref.getAll();
+
+        for (String k : Util.getSortedMapKey(otpSecrets)) {
+            try {
+                OTPSecret otpSecret = new OTPSecret((String)otpSecrets.get(k));
+                TextView textViewOTPIdentifier = new TextView(getContext());
+                textViewOTPIdentifier.setText(otpSecret.getIdentifier());
+                textViewOTPIdentifier.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+                binding.linearLayoutListOtp.addView(textViewOTPIdentifier);
+
+                TextView textViewOTP = new TextView(getContext());
+                textViewOTP.setText(otpSecret.getIdentifier());
+                textViewOTP.setText(otpSecret.getOTP());
+                textViewOTP.setTextSize(TypedValue.COMPLEX_UNIT_SP, 36);
+                binding.linearLayoutListOtp.addView(textViewOTP);
+
+                TextView divider = new TextView(getContext());
+                divider.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);
+                binding.linearLayoutListOtp.addView(divider);
+            } catch (Exception e) {
+                Log.v("ListOTPFragment", e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
--- app/src/main/java/com/example/otpgeneratorwithrecovery/util/Util.java
+++ app/src/main/java/com/example/otpgeneratorwithrecovery/util/Util.java
@@ -1,5 +1,8 @@
 package com.example.otpgeneratorwithrecovery.util;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 public class Util {
@@ -12,5 +15,19 @@ public class Util {
             max = Math.max(max, Integer.valueOf(entry.getKey()));
         }
         return String.valueOf(max + 1);
+    }
+
+    public static List<String> getSortedMapKey(Map<String, ?> sharedPreferenceAllValues) {
+        List<Integer> idList = new ArrayList<>();
+        for (Map.Entry<String,?> entry : sharedPreferenceAllValues.entrySet()) {
+            idList.add(Integer.valueOf(entry.getKey()));
+        }
+        Collections.sort(idList);
+
+        List<String> res = new ArrayList<>();
+        for (Integer id : idList) {
+            res.add(String.valueOf(id));
+        }
+        return res;
     }
 }
