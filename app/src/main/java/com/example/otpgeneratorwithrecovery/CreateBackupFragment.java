ef1e5da4d37b5cf74e00b36e606706a6df7d5d55
Prajna Prasetya <prajnapras19@gmail.com>
2023-04-20 16:12:21 +0700

    feat: skeleton for create backup

---
+++ app/src/main/java/com/example/otpgeneratorwithrecovery/CreateBackupFragment.java
@@ -0,0 +1,10 @@
+package com.example.otpgeneratorwithrecovery;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Create backup with SharedSecret class
+ */
+public class CreateBackupFragment extends Fragment {
+    // TODO
+}
