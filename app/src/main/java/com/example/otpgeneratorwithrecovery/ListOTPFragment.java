dd7d906f3e3ca79aa85ec60d3dd617fec9cf4ab5
Prajna Prasetya <prajnapras19@gmail.com>
2023-04-20 14:41:28 +0700

    feat: delete otp secret

--- app/src/main/java/com/example/otpgeneratorwithrecovery/ListOTPFragment.java
+++ app/src/main/java/com/example/otpgeneratorwithrecovery/ListOTPFragment.java
@@ -1,6 +1,8 @@
 package com.example.otpgeneratorwithrecovery;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,6 +11,7 @@ import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -73,6 +76,40 @@ public class ListOTPFragment  extends Fragment {
                 textViewOTPRemainingTime.setText(String.format("Time remaining until next OTP: %s seconds", Util.getRemainingOTPTime(otpSecret.getPeriod())));
                 textViewOTPRemainingTime.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
                 binding.linearLayoutListOtp.addView(textViewOTPRemainingTime);
+
+                Button buttonDeleteSecret = new Button(getContext());
+                buttonDeleteSecret.setText("Delete");
+                buttonDeleteSecret.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+                buttonDeleteSecret.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                        alert.setTitle("Delete");
+                        alert.setMessage("Are you sure you want to delete this secret?");
+                        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                SharedPreferences sharedPref = getContext().getSharedPreferences(getString(R.string.otp_secret_shared_preferences_file), Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPref.edit();
+                                editor.remove(k);
+                                editor.apply();
+                                dialog.dismiss();
+                            }
+                        });
+
+                        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                        alert.show();
+                    }
+                });
+                binding.linearLayoutListOtp.addView(buttonDeleteSecret);
 
                 TextView divider = new TextView(getContext());
                 divider.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);
