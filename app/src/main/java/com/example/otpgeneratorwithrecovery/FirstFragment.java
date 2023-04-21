efd2498e8a80b0c55260e48f44c2436c807044c4
Prajna Prasetya <prajnapras19@gmail.com>
2023-04-22 02:13:15 +0700

    feat: skeleton for manual recovery

--- app/src/main/java/com/example/otpgeneratorwithrecovery/FirstFragment.java
+++ app/src/main/java/com/example/otpgeneratorwithrecovery/FirstFragment.java
@@ -97,6 +97,14 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_RecoverOTPByScanQRCodeFragment);
             }
         });
+
+        binding.buttonFirstToRecoverOtpManualInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_RecoverOTPManualInputFragment);
+            }
+        });
     }
 
     @Override
