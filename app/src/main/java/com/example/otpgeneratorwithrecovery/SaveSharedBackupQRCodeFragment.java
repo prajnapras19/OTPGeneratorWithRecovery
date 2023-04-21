1c4415dcc1f6cab843f09b1b0a1eebb8c2c0c7cb
Prajna Prasetya <prajnapras19@gmail.com>
2023-04-21 12:11:10 +0700

    feat: save shared backup qr code

--- app/src/main/java/com/example/otpgeneratorwithrecovery/SaveSharedBackupQRCodeFragment.java
+++ app/src/main/java/com/example/otpgeneratorwithrecovery/SaveSharedBackupQRCodeFragment.java
@@ -1,14 +1,21 @@
 package com.example.otpgeneratorwithrecovery;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.otpgeneratorwithrecovery.crypto.SharedSecretToRecover;
 import com.example.otpgeneratorwithrecovery.databinding.FragmentOtpSecretQrCodeScannerBinding;
 import com.example.otpgeneratorwithrecovery.permissionlistener.AskForPermissionBackgroundThreadPermissionListener;
 import com.example.otpgeneratorwithrecovery.permissionlistener.AskForPermissionErrorListener;
+import com.example.otpgeneratorwithrecovery.util.Util;
 import com.google.zxing.Result;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.listener.PermissionRequestErrorListener;
@@ -52,7 +59,29 @@ public class SaveSharedBackupQRCodeFragment extends NeedPermissionFragment imple
 
     @Override
     public void handleResult(Result rawResult) {
-        // TODO
+        try {
+            SharedSecretToRecover sharedSecret = new SharedSecretToRecover(rawResult.getText());
+
+            SharedPreferences sharedPref = getContext().getSharedPreferences(getString(R.string.shared_backup_shared_preferences_file), Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString(Util.getNextSharedPreferenceID(sharedPref.getAll()), sharedSecret.toString());
+            editor.apply();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setTitle("Success");
+            builder.setMessage("Backup saved successfully for " + sharedSecret.getIdentifier());
+            AlertDialog alert1 = builder.create();
+            alert1.show();
+
+            NavHostFragment.findNavController(SaveSharedBackupQRCodeFragment.this).navigateUp();
+        } catch (Exception e) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setTitle("Error");
+            builder.setMessage(e.getMessage());
+            AlertDialog alert1 = builder.create();
+            alert1.show();
+            this.mScannerView.resumeCameraPreview(this);
+        }
     }
 
     @Override
