ed9d9dfddbe235ce505ca15e2709050ba355813b
Prajna Prasetya <prajnapras19@gmail.com>
2023-04-21 22:05:13 +0700

    feat: two section in recover with qr code

--- app/src/main/java/com/example/otpgeneratorwithrecovery/RecoverOTPByScanQRCodeFragment.java
+++ app/src/main/java/com/example/otpgeneratorwithrecovery/RecoverOTPByScanQRCodeFragment.java
@@ -1,17 +1,28 @@
 package com.example.otpgeneratorwithrecovery;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 
 import com.example.otpgeneratorwithrecovery.databinding.FragmentRecoverOtpByScanQrCodeBinding;
+import com.example.otpgeneratorwithrecovery.permissionlistener.AskForPermissionBackgroundThreadPermissionListener;
+import com.example.otpgeneratorwithrecovery.permissionlistener.AskForPermissionErrorListener;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-public class RecoverOTPByScanQRCodeFragment extends Fragment {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class RecoverOTPByScanQRCodeFragment extends NeedPermissionFragment implements ZXingScannerView.ResultHandler  {
     private FragmentRecoverOtpByScanQrCodeBinding binding;
+    private ZXingScannerView mScannerView;
+    private PermissionListener cameraPermissionListener;
+    private PermissionRequestErrorListener errorListener;
 
     @Override
     public View onCreateView(
@@ -19,17 +30,44 @@ public class RecoverOTPByScanQRCodeFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         binding = FragmentRecoverOtpByScanQrCodeBinding.inflate(inflater, container, false);
+        this.askForPermission();
         return binding.getRoot();
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void initScanner() {
+        mScannerView = new ZXingScannerView(getContext());
+        mScannerView.setAutoFocus(true);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+        binding.frameLayoutCamera.addView(mScannerView);
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
         // TODO
+    }
+
+    @Override
+    public void handlePermissionGranted() {
+        initScanner();
+    }
+
+    public void askForPermission() {
+        cameraPermissionListener = new AskForPermissionBackgroundThreadPermissionListener(this);
+        errorListener = new AskForPermissionErrorListener();
+
+        new Thread(() -> Dexter.withContext(getContext())
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(cameraPermissionListener)
+                .withErrorListener(errorListener)
+                .onSameThread()
+                .check()).start();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        mScannerView = null;
     }
 }
