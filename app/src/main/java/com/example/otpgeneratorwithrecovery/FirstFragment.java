a41d52b3a1e1d81ba881965225594956285ec424
Prajna Prasetya <prajnapras19@gmail.com>
2023-04-21 17:16:49 +0700

    feat: skeleton for list shared backup

--- app/src/main/java/com/example/otpgeneratorwithrecovery/FirstFragment.java
+++ app/src/main/java/com/example/otpgeneratorwithrecovery/FirstFragment.java
@@ -81,6 +81,14 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SaveSharedBackupManualInputFragment);
             }
         });
+
+        binding.buttonFirstToListSharedBackup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_ListSharedBackupFragment);
+            }
+        });
     }
 
     @Override
