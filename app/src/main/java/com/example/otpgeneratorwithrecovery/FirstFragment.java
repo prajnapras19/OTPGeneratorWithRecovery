905505b3ab141526c6246e8ecda65fcef73da28a
Prajna Prasetya <prajnapras19@gmail.com>
2023-04-22 15:29:52 +0700

    fix: manual input recovery is missing

--- app/src/main/java/com/example/otpgeneratorwithrecovery/FirstFragment.java
+++ app/src/main/java/com/example/otpgeneratorwithrecovery/FirstFragment.java
@@ -104,6 +104,14 @@ public class FirstFragment extends Fragment {
             }
         });
 
+        binding.buttonFirstToRecoverOtpManualInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_RecoverOTPManualInputFragment);
+            }
+        });
+
         binding.buttonFirstToShowClientIdentity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
