7366717c3d6dcc2e2cba1b6cf582a1a5247aad8c
Prajna Prasetya <prajnapras19@gmail.com>
2023-04-22 17:38:02 +0700

    feat: add friends by manual input

--- app/src/main/java/com/example/otpgeneratorwithrecovery/AddFriendByManualInputFragment.java
+++ app/src/main/java/com/example/otpgeneratorwithrecovery/AddFriendByManualInputFragment.java
@@ -1,5 +1,8 @@
 package com.example.otpgeneratorwithrecovery;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,8 +10,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.otpgeneratorwithrecovery.crypto.OTPFriend;
 import com.example.otpgeneratorwithrecovery.databinding.FragmentAddFriendByManualInputBinding;
+import com.example.otpgeneratorwithrecovery.util.Base32Wrapper;
+import com.example.otpgeneratorwithrecovery.util.Util;
 
 public class AddFriendByManualInputFragment extends Fragment {
     private FragmentAddFriendByManualInputBinding binding;
@@ -24,6 +31,33 @@ public class AddFriendByManualInputFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    OTPFriend friend = new OTPFriend(Base32Wrapper.decodeStringToString(binding.friendClientIdentity.getText().toString()));
+                    friend.setName(binding.name.getText().toString());
+
+                    SharedPreferences sharedPref = getContext().getSharedPreferences(getString(R.string.friends_shared_preferences_file), Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString(Util.getNextSharedPreferenceID(sharedPref.getAll()), friend.toString());
+                    editor.apply();
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setTitle("Success");
+                    builder.setMessage(String.format("Friend %s added successfully", friend.getName()));
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                    NavHostFragment.findNavController(AddFriendByManualInputFragment.this).navigateUp();
+                } catch (Exception e) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setTitle("Error");
+                    builder.setMessage(e.getMessage());
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+            }
+        });
     }
 
     @Override
